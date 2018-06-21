88787657c665f00052aa99945661a8c90779f06a
Ingo Fischer <github@fischer-ka.de>
2018-06-21 10:36:31 +0200

    try end clean because scheduled

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+/* jshint -W097 */
+/* jshint strict: false */
+/* jslint node: true */
+/* jslint esversion: 6 */
 'use strict';
+
+const utils   = require(__dirname + '/lib/utils'); // Get common adapter utils
 const IOB_HB = require(require.resolve('iobroker.ham'));
 const pack = require('./io-package.json');
 IOB_HB(pack.common.name);
+
+// To end if scheduled
+const adapter = new utils.Adapter(pack.common.name);
+setTimeout(() => adapter && adapter.stop(), 10000);
