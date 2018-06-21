7c9698b363507d5e570a40a8ae29cbff824eef7e
Ingo Fischer <github@fischer-ka.de>
2018-06-21 11:14:26 +0200

    works again?

--- main.js
+++ main.js
@@ -4,11 +4,11 @@
 /* jslint esversion: 6 */
 'use strict';
 
-const utils   = require(__dirname + '/lib/utils'); // Get common adapter utils
+//const utils   = require(__dirname + '/lib/utils'); // Get common adapter utils
 const IOB_HB = require(require.resolve('iobroker.ham'));
 const pack = require('./io-package.json');
 IOB_HB(pack.common.name);
 
 // To end if scheduled
-const adapter = new utils.Adapter(pack.common.name);
-setTimeout(() => adapter && adapter.stop(), 10000);
+//const adapter = new utils.Adapter(pack.common.name);
+//setTimeout(() => adapter && adapter.stop(), 10000);
